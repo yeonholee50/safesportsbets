4edc4997e16df516d091bf384b4cfd44ea282983
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2024-05-10 09:36:36 -0400

    implemented receive league results, odds and te name of leagues is in league_key.txt

--- backend/receive/receive_league_results.py
+++ backend/receive/receive_league_results.py
@@ -1,20 +1,33 @@
 import requests
 
 
+"""
+This is a one time use function the key, group, title, description, active, and has_outrights into a log
+"""
 def main():
+    scores_by_key = {}
+    with open("league_keys.txt", "r") as league_txt_file:
+        lines = league_txt_file.readlines()
+        
+        for line in lines:
+            line = line.split()
+            key, group, title, active, has_outright = line[0], line[1], line[2], line[-2], line[-1]
+            if active == 'True':
+                url = f"https://odds.p.rapidapi.com/v4/sports/{key}/scores"
+
+                querystring = {"regions":"us","oddsFormat":"decimal","markets":"h2h,spreads","dateFormat":"iso"}
+
+                headers = {
+                    "X-RapidAPI-Key": "cf36c28ab2msh3578c681fa5368cp168942jsnfe34a3f1abc4",
+                    "X-RapidAPI-Host": "odds.p.rapidapi.com"
+                }
+
+                response = requests.get(url, headers=headers, params=querystring)
+                odds = response.json()
+                scores_by_key[key] = odds
+        return scores_by_key
+
     
-    url = "https://odds.p.rapidapi.com/v4/sports/upcoming/odds"
-
-    querystring = {"regions":"us","oddsFormat":"decimal","markets":"h2h,spreads","dateFormat":"iso"}
-
-    headers = {
-        "X-RapidAPI-Key": "cf36c28ab2msh3578c681fa5368cp168942jsnfe34a3f1abc4",
-        "X-RapidAPI-Host": "odds.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers, params=querystring)
-
-    print(response.json())
 
 
 if __name__ == "__main__":
