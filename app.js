2702a2e85dd3a1a152c65c8701644249557f493d
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2024-05-15 16:46:43 -0400

    drop down should come down when hover now

--- app.js
+++ app.js
@@ -19,30 +19,39 @@ document.addEventListener('DOMContentLoaded', () => {
                 leaguesBySport[sport] = [leagueKey];
             }
         });
-        console.log(leaguesBySport);
+        
         return leaguesBySport;
     }
 
     function displayNavigation(leaguesBySport) {
         const navBar = document.getElementById('nav-bar');
         for (const sport in leaguesBySport) {
-            const sportLink = document.createElement('div');
-            sportLink.textContent = sport;
-            sportLink.classList.add('sport-link');
+            const sportItem = document.createElement('li');
+            sportItem.textContent = sport;
     
             const leagueList = document.createElement('ul');
             leagueList.classList.add('league-list');
+    
             leaguesBySport[sport].forEach(league => {
                 const leagueItem = document.createElement('li');
                 leagueItem.textContent = league;
                 leagueList.appendChild(leagueItem);
             });
     
-            sportLink.appendChild(leagueList);
+            sportItem.appendChild(leagueList);
+            navBar.appendChild(sportItem);
     
-            navBar.appendChild(sportLink);
+            // Show/hide league list when hovering over sportItem
+            sportItem.addEventListener('mouseenter', () => {
+                leagueList.style.display = 'block';
+            });
+    
+            sportItem.addEventListener('mouseleave', () => {
+                leagueList.style.display = 'none';
+            });
         }
     }
+    
     
 
     function displayLeagues(leagues) {
