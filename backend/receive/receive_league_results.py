7b620511accf6655ebeab549029c8894ca43975e
Yeon Lee <yeonholee50@gmail.com>
2024-07-06 14:06:50 -0400

    redid mongo

--- backend/receive/receive_league_results.py
+++ backend/receive/receive_league_results.py
@@ -1,33 +1,37 @@
 import requests
-import os
-import sys
 from pymongo import MongoClient
-
 """
-This is a once per day operation
+This is a one time use function the key, group, title, description, active, and has_outrights into a log
 """
 def get_database():
-    connection_string = "mongodb+srv://yeonholee50:YeonHo1010@cluster0.j2eo96c.mongodb.net/"
+    connection_string = "mongodb+srv://yeonholee50:<password>@cluster0.j2eo96c.mongodb.net/"
     client = MongoClient(connection_string)
     return client
 def main():
-    client = get_database()
-    db = client.sports
-    coll = db.league_names
-    cursor = coll.find()
-    sport_names = set()
-    for doc in cursor:
-        if (doc['key'], doc['active']) not in sport_names:
-            sport_names.add((doc['key'], doc['active']))
-    client.close()
-    """
-    Now we store today's odds into db
-    """
-    inserted_ids = []
-    client = get_database()
-    db = client.sports
-    coll = db.results
+    scores_by_key = {}
+    with open("league_keys.txt", "r") as league_txt_file:
+        lines = league_txt_file.readlines()
+        
+        for line in lines:
+            line = line.split()
+            key, group, title, active, has_outright = line[0], line[1], line[2], line[-2], line[-1]
+            if active == 'True':
+                url = f"https://odds.p.rapidapi.com/v4/sports/{key}/scores"
+
+                querystring = {"regions":"us","oddsFormat":"decimal","markets":"h2h,spreads","dateFormat":"iso"}
+
+                headers = {
+                    "X-RapidAPI-Key": "cf36c28ab2msh3578c681fa5368cp168942jsnfe34a3f1abc4",
+                    "X-RapidAPI-Host": "odds.p.rapidapi.com"
+                }
+
+                response = requests.get(url, headers=headers, params=querystring)
+                odds = response.json()
+                scores_by_key[key] = odds
+        return scores_by_key
+
     
+<<<<<<< HEAD
     for sport_name in sport_names:
         url = f"https://odds.p.rapidapi.com/v4/sports/{sport_name[0]}/scores"
 
@@ -52,6 +56,8 @@ def main():
     client.close()
     return inserted_ids
       
+=======
+>>>>>>> parent of ec60837 (finished results api)
 
 
 if __name__ == "__main__":
