f4915f1bf6a1d8c6c9d5bfc7580316d0f29aef2e
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2024-07-30 11:55:32 -0400

    Update sportSeed.js

--- seeders/sportSeed.js
+++ seeders/sportSeed.js
@@ -3,7 +3,7 @@ const db = require('../models/sport');
 require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/sportsbook4",
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
