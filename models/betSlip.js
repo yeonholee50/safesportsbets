a5c3b9aafdc916ee65744e89144c3acfffcebbed
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2024-07-30 11:53:53 -0400

    Update betSlip.js

--- models/betSlip.js
+++ models/betSlip.js
@@ -25,7 +25,6 @@ const betSlipSchema = new Schema({
 betSlipSchema.post('findOneAndUpdate', (slip) => {
   const completed = slip.quantity.completed;
   const totalBets = slip.quantity.total;
-  // console.log(`${ completed }/${ totalBets }`)
 
   if (totalBets === completed || slip.outcome === false) {
     slip.status = "Completed"
@@ -37,7 +36,6 @@ betSlipSchema.post('findOneAndUpdate', (slip) => {
       slip.save();
     }
   }
-  // return slip;
 });
 
 const BetSlip = mongoose.model("BetSlip", betSlipSchema);
